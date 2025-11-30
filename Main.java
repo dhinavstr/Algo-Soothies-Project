f60cf26d503c53810e010440b5ab045ca7ecf95f
Dhina Novitasari <dhinanvstr@gmail.com>
2025-11-30 18:53:08 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -4,20 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== SELECT ROLE ===");
-        System.out.println("1. Owner");
-        System.out.println("2. Customer");
-        System.out.print("Pilih: ");
-        int role = sc.nextInt();
+        System.out.println("========================================");
+        System.out.println("     SELAMAT DATANG DI ALGO SMOOTHIE");
+        System.out.println("========================================");
 
-        if (role == 1) {
-            OwnerFlow.startOwnerMenu();
-        } else if (role == 2) {
-            CustomerFlow.startCustomerFlow();
-
-        } else {
-            System.out.println("Pilihan tidak valid.");
-        }
+        CustomerFlow.startCustomerFlow();
     }
 }
-
